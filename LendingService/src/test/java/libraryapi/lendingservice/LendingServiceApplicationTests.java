01cb7fe2fa5d279ef597b5da1601c43d91c1f238
josefrancisco17 <josefranciscoantunesleal@gmail.com>
2024-10-14 10:40:37 +0100

    File structure

---
+++ LendingService/src/test/java/libraryapi/lendingservice/LendingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package libraryapi.lendingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ReaderService/src/test/java/libraryapi/readerservice/ReaderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package libraryapi.readerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
