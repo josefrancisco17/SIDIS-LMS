dda311bbde9f64d7962b76ebc144003951251cb4
Fonseca <franciscoloureirofonseca@gmail.com>
2024-12-01 21:35:51 +0000

    ServiceImplTests

---
+++ BookServiceQuery/src/test/java/libraryapi/bookservicequery/services/BookServiceImplTest.java
@@ -0,0 +1,55 @@
+package libraryapi.bookservicequery.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BookServiceImplTest {
+
+    @Test
+    void getBook() {
+    }
+
+    @Test
+    void getBooks() {
+    }
+
+    @Test
+    void getAllBooks() {
+    }
+
+    @Test
+    void getTopGenres() {
+    }
+
+    @Test
+    void getTopBooks() {
+    }
+
+    @Test
+    void getBooksByAuthorId() {
+    }
+
+    @Test
+    void getBooksByGenre() {
+    }
+
+    @Test
+    void getBooksByTitle() {
+    }
+
+    @Test
+    void getBooksByAuthor() {
+    }
+
+    @Test
+    void getBooksByTitleAndGenreAndAuthor() {
+    }
+
+    @Test
+    void getBookCover() {
+    }
+
+    @Test
+    void manageInternalBook() {
+    }
+}
---
+++ ReaderServiceCommand/src/test/java/libraryapi/readerservicecommand/services/ReaderServiceImplTest.java
@@ -0,0 +1,23 @@
+package libraryapi.readerservicecommand.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ReaderServiceImplTest {
+
+    @Test
+    void createReader() {
+    }
+
+    @Test
+    void updateReader() {
+    }
+
+    @Test
+    void partialUpdateReader() {
+    }
+
+    @Test
+    void doUploadFile() {
+    }
+}
