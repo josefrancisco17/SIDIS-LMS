b8fe37ef6d666fd6465e1f614818c55c2031f7fd
Fonseca <franciscoloureirofonseca@gmail.com>
2024-12-01 21:45:23 +0000

    ServiceImplTests Update

--- BookServiceQuery/src/test/java/libraryapi/bookservicequery/services/GenreServiceImplTest.java
+++ BookServiceQuery/src/test/java/libraryapi/bookservicequery/services/GenreServiceImplTest.java
@@ -23,44 +23,35 @@ class GenreServiceImplTest {
 
     @BeforeEach
     void setUp() {
-        // Inicializa os mocks
         MockitoAnnotations.openMocks(this);
 
-        // Inicializa a implementação do serviço, injetando o repositório mockado
         genreService = new GenreServiceImpl(genreRepository);
     }
 
     @Test
     void getGenreById() {
-        // Configura o mock para o comportamento esperado
         Genre genre = new Genre(1L, "Fiction");
         when(genreRepository.findGenreById(1L)).thenReturn(Optional.of(genre));
 
-        // Chama o método a ser testado
         Optional<Genre> result = genreService.getGenreById(1L);
 
-        // Verifica se o retorno é correto
         assertTrue(result.isPresent());
         assertEquals("Fiction", result.get().getName());
     }
 
     @Test
     void getAllGenres() {
-        // Configura o mock para o comportamento esperado
         Genre genre1 = new Genre(1L, "Fiction");
         Genre genre2 = new Genre(2L, "Non-Fiction");
 
-        // Usando ArrayList para converter Iterable para List
         List<Genre> genres = new ArrayList<>();
         genres.add(genre1);
         genres.add(genre2);
 
         when(genreRepository.findAll()).thenReturn(genres);
 
-        // Chama o método a ser testado
         Iterable<Genre> result = genreService.getAllGenres();
 
-        // Verifica se o retorno é correto
         assertNotNull(result);
         assertTrue(result.iterator().hasNext());
         assertEquals("Fiction", result.iterator().next().getName());
